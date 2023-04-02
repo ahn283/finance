6e734ce7195ea13af081ea7d8994b7a0774bb0b2
ahn283 <ahn283@gmail.com>
2023-04-02 22:49:39 +0900

    adding module files

--- machine_learning/module_regression.py
+++ machine_learning/module_regression.py
@@ -99,3 +99,71 @@ def scale(scaler, X_train,X_test):
     X_test_scaling = pd.DataFrame(scaler_fit.transform(X_test), index=X_test.index, columns=X_test.columns)
 
     return X_test_scaling, X_test_scaling
+
+# 실제 Y와 예측치 시각화
+def plot_prediction(Y_true_pred):
+    plt.figure(figsize=(16, 8))
+    plt.plot(Y_true_pred, linewidth=5, label=Y_true_pred.columns)
+    plt.xticks(fontsize=25, rotation=0)
+    plt.yticks(fontsize=25)
+    plt.xlabel('Index', fontname='serif', fontsize=28)
+    plt.legend(fontsize=20)
+    plt.grid()
+    plt.show()
+
+# 검증 함수화
+def evaluation_reg(Y_real, Y_pred):
+    MAE = mean_absolute_error(Y_real, Y_pred)
+    MSE = mean_squared_error(Y_real, Y_pred)
+    MAPE = mean_absolute_percentage_error(Y_real, Y_pred)
+    Score = pd.DataFrame([MAE, MSE, MAPE], index=['MAE', 'MSE', 'MAPE'], columns=['Score']).T
+
+    return Score
+
+# Train & Test 모두의 검증 함수화
+def evaluation_reg_trte(Y_real_tr, Y_pred_tr, Y_real_te, Y_pred_te):
+    Score_tr = evaluation_reg(Y_real_tr, Y_pred_tr)
+    Score_te = evaluation_reg(Y_real_te, Y_pred_te)
+    Score_trte = pd.concat([Score_tr, Score_te], axis=0)
+    Score_trte.index = ['Train', 'Test']
+
+    return Score_trte
+
+# 에러 분석
+def error_analysis(X_Data, Y_Pred, Residual, graph_on=False):
+    if graph_on == True:
+        #### 시각화
+        # 잔차의 정규분포성 확인
+        sns.displot(Residual, norm_hist='True', fit=stats.norm)
+        plt.show()
+
+        # 잔차의 등분산성 확인
+        temp = pd.concat([Y_Pred, Residual.reset_index().iloc[:, [1]]], axis=1)
+        sns.scatterplot(x='Pred', y='Error', data=temp)
+        plt.show()
+
+        # 잔차의 자기상관성 확인
+        sm.graphics.tsa.plot_acf(Residual, lags=50, use_vlines=True)
+
+    #### 통계량
+    # 정규분포
+    # Null Hypothesis : The residuals are normally distributed
+    Normality = pd.DataFrame([stats.shapiro(Residual)], 
+                             index=['Normality'], columns=['Test Statistics', 'p-value']).T
+    
+    # 등분산성
+    # Null hypothesis : Error terms are homoscedastic
+    Heteroscedasticity = pd.DataFrame([sm.stats.diagnostic.het_goldfeldquandt(Residual, X_Data.values, alternative='two-sided')],
+                                      index=['Heteroscedasticity'],
+                                      columns=['Test Statistics', 'p-value', 'Altertive']).T
+    
+    # 자기상관
+    # Null Hypothesis: Autocorrelation is absent
+    Autocorrelation = pd.concat([pd.DataFrame(sm.stats.diagnostic.acorr_ljungbox(Residual, lags=[10, 50]).iloc[:, 0]),
+                                 pd.DataFrame(sm.stats.diagnostic.acorr_ljungbox(Residual, lags=[10,50]).iloc[:, 1])], axis=1).T
+    Autocorrelation.index = ['Test Statistics', 'p-value']
+    Autocorrelation.columns = ['Autocorr(lags10)', 'Autocorr(lag50)']
+
+    Error_Analysis = pd.concat([Normality, Heteroscedasticity, Autocorrelation], join='outer', axis=1)
+
+    return Error_Analysis
