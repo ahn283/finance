6a30e696ee5d670934964c9bbe5011cac4e5c36b
ahn283 <ahn283@gmail.com>
2023-07-31 09:22:40 +0900

    complete asset allocation

---
+++ python_for_finance_cookbook/asset_allocation_utils.py
@@ -0,0 +1,19 @@
+def print_portfolio_summary(perf, weights, assets, name):
+    """
+    Helper function for printing the performance summay of a portfolio
+
+
+    Args:
+        perf (pd.Series): Series containing the perf metrics
+        weights (np.array) : An array containing the weights of the portfolio
+        assets (list) : list of the asset names
+        name (str) : The name of the portfolio
+    """
+
+    print(f"{name} portfolio ----")
+    print("Performance")
+    for index, value in perf.items():
+        print(f"{index} : {100 * value:.2f}% ", end="", flush=True)
+    print("\nWeights")
+    for x, y in zip(assets, weights):
+        print(f"{x} : {100 * y:.2f}% ", end="", flush=True)
