7e33c53a6237784c00b6c720b2d3cef8d9ccc18a
Woojin Ahn <ahn283@gmail.com>
2024-01-27 23:55:46 +0900

    update

--- reinforcement_learning/fast_campus/part4/ch2_td_prediction/example1/environment.py
+++ reinforcement_learning/fast_campus/part4/ch2_td_prediction/example1/environment.py
@@ -5,73 +5,58 @@ import numpy as np
 class Env:
     def __init__(self):
         '''
-        state_space: 3x4 grid info using numpy
+        state_space : 4x4 grid info using numpy
         value of the agent location: 1
         value of the goal location: -1
         
-        action_space = {0, 1, 2, 3}
+        action_space: {0, 1, 2, 3}
         0: up
         1: right
         2: down
         3: left
         '''
-        self.agent_pos = {'y': 0, 'x': 0}
-        self.goal_pos = {'y': 0, 'x': 3}
-        self.drift_area = {'y': [0], 'x': [1,2]}
-        self.y_min, self.x_min, self.y_max, self.x_max = 0, 0, 2, 3
+        
+        self.agent_pos = {'y':0, 'x':0}
+        self.goal_pos = {'y':3, 'x':3}
+        self.y_min, self.x_min, self.y_max, self.x_max = 0, 0, 3, 3
         
         # set up state
-        self.state = np.zeros([3, 4])
+        self.state = np.zeros([4, 4])
         self.state[self.goal_pos['y'], self.goal_pos['x']] = -1
-        self.state[self.drift_area['y'], self.drift_area['x']] = -2
         self.state[self.agent_pos['y'], self.agent_pos['x']] = 1
         
         self.state_space = list()
-        for y in range(3):
+        for y in range(4):
             for x in range(4):
-                if (
-                    y in self.drift_area['y'] and
-                    x in self.drift_area['x']
-                ):
-                    continue
-                state = np.zeros([3, 4])
+                state = np.zeros([4, 4])
                 state[self.goal_pos['y'], self.goal_pos['x']] = -1
-                state[self.drift_area['y'], self.drift_area['x']] = -2
                 state[y, x] = 1
                 self.state_space.append(state)
+                
         self.action_space = [0, 1, 2, 3]
         
     def reset(self):
         self.agent_pos = {'y':0, 'x': 0}
-        self.state = np.zeros([3, 4])
+        self.state = np.zeros([4, 4])
         self.state[self.goal_pos['y'], self.goal_pos['x']] = -1
-        self.state[self.drift_area['y'], self.drift_area['x']] = -2
         self.state[self.agent_pos['y'], self.agent_pos['x']] = 1
         
         return self.state
     
     def exploring_reset(self):
         while True:
-            y_random = np.random.randint(3)
+            y_random = np.random.randint(4)
             x_random = np.random.randint(4)
-            self.agent_pos['y'] = y_random
-            self.agent_pos['x'] = x_random
-            if not (
-                self.agent_pos == self.goal_pos or
-                (y_random in self.drift_area['y'] and 
-                 x_random in self.drift_area['x'])
-            ):
+            self.agent_pos = {'y': y_random, 'x': x_random}
+            if self.agent_pos != self.goal_pos:
                 break
-        
-        self.state = np.zeros([3, 4])
+        self.state = np.zeros([4, 4])
         self.state[self.goal_pos['y'], self.goal_pos['x']] = -1
-        self.state[self.drift_area['y'], self.drift_area['x']] = -2
         self.state[self.agent_pos['y'], self.agent_pos['x']] = 1
-        
         return self.state
     
     def step(self, action):
-        # update environment variables
+        # update environmental variables
         if action == 0:
             # 'y' should be decreased by 1 or stay the same when it is at the top row
             self.agent_pos['y'] = max(
@@ -85,13 +70,13 @@ class Env:
                 self.x_max
             )
         elif action == 2:
-            # 'y' should be increased by 1 or stay the same when it is at the bottom row
+            # 'y' should be increased by 1 or stay the same when it is at the most left column
             self.agent_pos['y'] = min(
                 self.agent_pos['y'] + 1,
                 self.y_max
             )
         elif action == 3:
-            # 'x' should be decreased by 1 or stay the same when it is at the most left column
+            # 'x' should be decreased by 1 or stay the same when it is a the most left column
             self.agent_pos['x'] = max(
                 self.agent_pos['x'] - 1,
                 self.x_min
@@ -99,24 +84,16 @@ class Env:
         else:
             assert False, "Invalid action value was fed to step."
             
-        if (
-            self.agent_pos['y'] in self.drift_area['y'] and
-            self.agent_pos['x'] in self.drift_area['x']
-        ):
-            self.agent_pos['y'] = 0
-            self.agent_pos['x'] = 0
-            
         # make a next state after transition
         prev_state = self.state
-        self.state = np.zeros([3, 4])
+        self.state = np.zeros([4, 4])
         self.state[self.goal_pos['y'], self.goal_pos['x']] = -1
-        self.state[self.drift_area['y'], self.drift_area['x']] = -2
         self.state[self.agent_pos['y'], self.agent_pos['x']] = 1
         
         done = False
         if self.agent_pos == self.goal_pos:
             done = True
-            
+        
         reward = self.reward(prev_state, action, self.state)
         
         return reward, self.state, done
@@ -126,16 +103,17 @@ class Env:
         y, x = np.where(s == 1)
         y_next, x_next = np.where(s_next == 1)
         if (
-            (y_next == self.goal_pos['y'] and
+            (y_next == self.goal_pos['y'] and 
              x_next == self.goal_pos['x']) and
-            (y != self.goal_pos['y'] or x != self.goal_pos['x'])
-        ):      # reward the goal
+            (y != self.goal_pos['y'] or 
+             x != self.goal_pos['x'])
+        ):      # reached the goal
             reward = 10
             
         return reward
     
     def transition_probability(self, s, a, s_next):
-        y, x = np.where(s == 1)     # get agent pos from s
+        y, x = np.where(s == 1)     # get agent pos
         y_next, x_next = np.where(s_next == 1)      # get agent pos from next_s
         
         # already reached goal
@@ -152,29 +130,23 @@ class Env:
             y_next_temp, x_next_temp = min(y + 1, self.y_max), x
         # left movement
         elif a == 3:
-            y_next_temp, x_next_temp = y, max(x -1, self.x_min)
+            y_next_temp, x_next_temp = y, max(x - 1, self.x_min)
         else:
             assert False, "Invalid action value was fed to step."
             
-        if (
-            y_next_temp in self.drift_area['y'] and
-            x_next_temp in self.drift_area['x']
-        ):
-            y_next_temp = 0
-            x_next_temp = 0
-            
-        is_correct_transition = (
+        is_correction_transition = (
             y_next_temp == y_next and
             x_next_temp == x_next
         )
         
-        if is_correct_transition:
+        if is_correction_transition:
             return 1.0
         else:
             return 0.0
         
 if __name__ == "__main__":
     import random
+    from IPython import embed
     
     env = Env()
     s = env.reset()
@@ -199,11 +171,11 @@ if __name__ == "__main__":
             s_next_ = random.sample(env.state_space, 1)[0]
         check2 = env.transition_probability(s, a, s_next_) == 0.0
         
-        check = check1 and check2 == True
+        check = (check1 and check2) == True
         
         if not check:
-            print("Somehting is wrong!")
+            print("Something is wrong!!")
+            embed()
             break
-    
-    print("Environment verification is done.")
         
+    print("Environment verification is done")
