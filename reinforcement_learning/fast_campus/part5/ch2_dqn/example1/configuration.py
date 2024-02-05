3dc64e005762c12dbf421d46504478d4173c7e90
Woojin Ahn <ahn283@gmail.com>
2024-02-06 00:43:44 +0900

    update

--- reinforcement_learning/fast_campus/part5/ch2_dqn/example1/configuration.py
+++ reinforcement_learning/fast_campus/part5/ch2_dqn/example1/configuration.py
@@ -1,6 +1,6 @@
 # configuration.py
 
-from utils import AttrDict
+from utils import AttrDict    # dict를 attribute로 호출할 수 있는 함수
 
 config = AttrDict(
     gamma=0.99,
@@ -8,12 +8,12 @@ config = AttrDict(
     batch_size=64,
     hidden_size=128,
     replay_capacity=10000,
-    replay_init_ratio=0.3,
+    replay_init_ratio=0.3,    # replay를 얼만큼 채우고 시작할지
     train_env_steps=200000,
     target_update_period=100,
     eps_init=1.0,
     eps_final=0.05,
     eps_decrease_step=100000,
-    num_val_episode=20,
-    eval_period=500,
+    num_val_episode=20,   # evaluation 횟수
+    eval_period=500,    # evaluation 주기
 )
--- reinforcement_learning/fast_campus/part5/ch2_dqn/example1/utils.py
+++ reinforcement_learning/fast_campus/part5/ch2_dqn/example1/utils.py
@@ -12,22 +12,26 @@ def create_env(config):
 
 
 class RewardShapingWrapper(gym.Wrapper):
+    # 원래 리워드에 0.1을 곱하는 wrapper
     def __init__(self, env):
         self.env = env
         
     def step(self, action):
+        # 첫번째 값이 1/(1-gamma)=100이라는 큰수를 가질 수 있으므로 학습의 안정성을 위해 1/10 적용
         s_next, r, done, info = self.env.step(action)
         r = 0.1 * r
         return s_next, r, done, info
     
 class TimeStepAppendWrapper(gym.Wrapper):
+    # 기존 상태 return에 timestep 추가해서 5개의 값을 return해주도록 변경
+    # agent의 환경을 더 잘 이해하기 위해 timestep을 추가
     def __init__(self, env):
         self.env = env
         self.step_count = 0
         self.observation_space = gym.spaces.Box(
-            np.concatenate([self.env.observation_space.low, [0]], axis=-1),
-            np.concatenate([self.env.observation_space.high, [5]], axis=-1),
-            (self.env.observation_space.shape[0] + 1, ),
+            np.concatenate([self.env.observation_space.low, [0]], axis=-1),  # 상태 최소값 = 0
+            np.concatenate([self.env.observation_space.high, [5]], axis=-1),  # 상태 최대값 = 500이지만 step_count에 0.01을 곱함
+            (self.env.observation_space.shape[0] + 1, ),    # dimension 기존 4 + 1
             np.float32
         )
     
@@ -44,9 +48,12 @@ class TimeStepAppendWrapper(gym.Wrapper):
         return s_next, r, done, info
     
 class AttrDict(dict):
+    # attribute를 설정하면 dict의 key, value 값 설정
     __setattr__ = dict.__setitem__
     
     def __getattribute__(self, item):
+        # attribute 가져오는 함수
+        # attribute가 key(item)로 존재하면 해당 key return
         if item in self:
             return self[item]
         else:
