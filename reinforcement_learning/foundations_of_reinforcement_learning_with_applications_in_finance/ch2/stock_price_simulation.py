5ec9110cd697ef20453144efef1286c4ad1dbc96
ahn283 <ahn283@gmail.com>
2024-02-18 00:12:21 +0900

    update books

---
+++ reinforcement_learning/foundations_of_reinforcement_learning_with_applications_in_finance/ch2/stock_price_simulation.py
@@ -0,0 +1,7 @@
+from dataclasses import dataclass
+from typing import Optional, Mapping, Sequence, Tuple
+from collections import Counter
+import numpy as np
+from numpy.random import binomial
+import itertools
+from operator import itemgetter
