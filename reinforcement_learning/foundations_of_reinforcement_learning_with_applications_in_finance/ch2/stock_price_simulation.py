1ea5212f69147ae6da70d1d1e4a2dc30435e229c
Woojin Ahn <ahn283@gmail.com>
2024-02-18 21:37:55 +0900

    update

--- reinforcement_learning/foundations_of_reinforcement_learning_with_applications_in_finance/ch2/stock_price_simulation.py
+++ reinforcement_learning/foundations_of_reinforcement_learning_with_applications_in_finance/ch2/stock_price_simulation.py
@@ -5,3 +5,25 @@ import numpy as np
 from numpy.random import binomial
 import itertools
 from operator import itemgetter
+from utils.common_funcs import get_logistic_func, get_unit_sigmoid_func
+
+@dataclass
+class Process1:
+    @dataclass
+    class State:
+        price: int
+        
+    level_param: int    # level to which price mean-reverts
+    alpha1: float = 0.25 # strength of mean-reversion (non-negative value)
+    
+    def up_prob(self, state: State) -> float:
+        return get_logistic_func(self.alpha1)(self.level_param - state.price)
+    
+    def next_state(self, state: State) -> State:
+        up_move: int = binomial(1, self.up_prob(state), 1)[0]
+        return Process1.State(price=state.price + up_move * 2 - 1)
+    
+handy_map : Mapping[Optional[bool], int] = {True: -1, False: 1, None: 0}
+
+
+
--- reinforcement_learning/foundations_of_reinforcement_learning_with_applications_in_finance/utils/common_funcs.py
+++ reinforcement_learning/foundations_of_reinforcement_learning_with_applications_in_finance/utils/common_funcs.py
@@ -7,3 +7,36 @@ def get_logistic_func(alpha: float) -> Callable[[float], float]:
 
 def get_unit_sigmoid_func(alpha: float) -> Callable[[float], float]:
     return lambda x: 1. / (1+ (1 / np.where(x == 0, VSML, x) - 1) ** alpha)
+
+
+if __name__ == "__main__":
+    from plot_funcs import plot_list_of_curves
+    
+    alpha = [2.0, 1.0, 0.5]
+    colors = ["r-", "b--", "g-."]
+    labels = [(r"$\alpha$ = %.1f" % a) for a in alpha]
+    logistics = [get_logistic_func(a) for a in alpha]
+    x_vals = np.arange(-3.0, 3.01, 0.05)
+    y_vals = [f(x_vals) for f in logistics]
+    plot_list_of_curves(
+        [x_vals] * len(logistics),
+        y_vals,
+        colors,
+        labels,
+        title="Logisitc Functions"
+    ) 
+    
+    alpha = [2.0, 1.0, 0.5]
+    colors = ["r-", "b--", "g-."]
+    labels = [(r"$\alpha$ = %.1f" % a) for a in alpha]
+    unit_sigmoids = [get_unit_sigmoid_func(a) for a in alpha]
+    x_vals = np.arange(0.0, 1.01, 0.01)
+    y_vals = [f(x_vals) for f in unit_sigmoids]
+    plot_list_of_curves(
+        [x_vals] * len(logistics),
+        y_vals,
+        colors,
+        labels,
+        title="Unit-Sigmoid Functions"
+    )
+    
